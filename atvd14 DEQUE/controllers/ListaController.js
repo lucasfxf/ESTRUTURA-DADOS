370cea18c36c783e17b05e72470e10d44eae21e4
Lucas <lucastrabalho171088@gmail.com>
2026-05-18 16:53:28 -0300

    Exercício DEQUE - Implementação de uma Deque (Double-Ended Queue) em JavaScript

--- atvd14 DEQUE/controllers/ListaController.js
+++ atvd14 DEQUE/controllers/ListaController.js
@@ -1,4 +1,5 @@
 const minhaLista = new LinkedList();
+
 //---------------------------------------------------------------------------------------------
 function limpaInputs() {
     document.getElementById("txtnovaTarefa").value = "";
@@ -6,18 +7,22 @@ function limpaInputs() {
     document.getElementById("txtIndice").value = "";
     document.getElementById("txtnovaTarefa").focus();
 }
+
 //--------------------------------------------------------------------------------------------
 function leiaDadosTarefa() {
     const descricao = document.getElementById("txtnovaTarefa").value.trim();
-    const prioridade = document.getElementById("txtnovaPrioridade").value.trim();
-    if (descricao === "" || prioridade === "") {
-        alert("Preencha os campos de descrição e prioridade!");
+    const prioridade = parseInt(document.getElementById("txtnovaPrioridade").value);
+
+    if (descricao === "" || isNaN(prioridade)) {
+        alert("Preencha os campos corretamente!");
         return null;
     }
+
     return new Tarefa(descricao, prioridade, obterDataAtual(), obterHoraAtual());
 }
+
 //--------------------------------------------------------------------------------------------
- function adicionarElementoInicio() {
+function adicionarElementoInicio() {
     const novaTarefa = leiaDadosTarefa();
     if (novaTarefa) {
         minhaLista.addFirst(novaTarefa);
@@ -25,70 +30,118 @@ function leiaDadosTarefa() {
         atualizarLista();
         limpaInputs();
     }
- }
- //------------------------------------------------------------------------------------------------------
-  function adicionarElementoFinal() {
-    const novaTarefa = leiaDadosTarefa();  
-    minhaLista.addLast(novaTarefa);
-    alert("Inserido!");
-    atualizarLista();
-    limpaInputs();
-  }
-  //--------------------------------------------------------------------------------------------
-  function adicionarIndice() {
-    //implemente
-  }
+}
+
+//------------------------------------------------------------------------------------------------------
+function adicionarElementoFinal() {
+    const novaTarefa = leiaDadosTarefa();
+    if (novaTarefa) {
+        minhaLista.addLast(novaTarefa);
+        alert("Inserido!");
+        atualizarLista();
+        limpaInputs();
+    }
+}
+
 //--------------------------------------------------------------------------------------------
- // Função para remover o primeiro elemento da lista
- function removerElementoInicio() {
-    if(!minhaLista.isEmpty()){
-      const tarefaRealizada = minhaLista.removeFirst();
-      mostrarMensagemRemocao(tarefaRealizada);
-      atualizarLista();
+function adicionarIndice() {
+    const novaTarefa = leiaDadosTarefa();
+    const index = parseInt(document.getElementById("txtIndice").value);
+
+    if (novaTarefa && !isNaN(index)) {
+        minhaLista.addAtIndex(novaTarefa, index);
+        alert("Inserido no índice!");
+        atualizarLista();
+        limpaInputs();
+    } else {
+        alert("Índice inválido!");
     }
-    else{
-      alert("Lista de Tarefas Vazia");
+}
+
+//--------------------------------------------------------------------------------------------
+// função pra adicionar tarefa ordenada por prioridade
+function adicionarTarefa() {
+    const novaTarefa = leiaDadosTarefa();
+
+    if (novaTarefa) {
+        minhaLista.addOrdenado(novaTarefa);
+        alert("Tarefa adicionada!");
+        atualizarLista();
+        limpaInputs();
     }
-   
- }
- //--------------------------------------------------------------------------------------------
- // Função para remover o ultimo elemento da lista
- function removerElementoFinal() {
-    if(!minhaLista.isEmpty()){
-      const tarefaRealizada = minhaLista.removeLast();
-      mostrarMensagemRemocao(tarefaRealizada);
-      atualizarLista();
+}
+
+//--------------------------------------------------------------------------------------------
+// Função para remover a primeira tarefa da lista (com tempo)
+function removerPrimeira() {
+    if (!minhaLista.isEmpty()) {
+        const tarefa = minhaLista.removeFirstComTempo();
+        mostrarMensagemRemocao(tarefa);
+        atualizarLista();
+    } else {
+        alert("Lista de Tarefas Vazia");
     }
-    else{
-      alert("Lista de Tarefas Vazia");
-    }
- }
+}
 
 //--------------------------------------------------------------------------------------------
 function mostrarMensagemRemocao(tarefaRealizada) {
     const mensagem = document.getElementById("mensagem-remocao");
-    mensagem.innerHTML ="Tarefa realizada: "+ tarefaRealizada.descricao;
+    mensagem.innerHTML = "Tarefa realizada: " + tarefaRealizada.descricao;
     mensagem.style.display = "block";
-  }
+}
+
 //-------------------------------------------------------------------------------------------- 
-// Função para atualizar a exibição da fila
- function atualizarLista() {
-   const listaTarefas = 
-       document.getElementById("list_listadeTarefas");
-   const lblTarefas = 
-          document.getElementById("lblmostraTarefas");
-   listaTarefas.innerHTML = "";    // limpar antes de mostrar
-   if(!minhaLista.isEmpty()){
-      lblTarefas.innerHTML = "Lista de Tarefas";
-      for(const tarefa of minhaLista){
-          const novaLinha = document.createElement("li");
-          novaLinha.innerHTML = tarefa.toString();
-          listaTarefas.appendChild(novaLinha);
-      }
-   }
-   else{
+// função pra mostrar a primeira tarefa da lista
+function mostrarPrimeira() {
+    const tarefa = minhaLista.getFirst();
+
+    if (!tarefa) {
+        alert("Lista vazia");
+        return;
+    }
+
+    alert("Primeira tarefa: " + tarefa.descricao);
+}
+
+//----------------------------------------------------------------------------------------------
+// função pra mostrar a tarefa mais antiga da lista
+function mostrarMaisAntiga() {
+    minhaLista.tarefaMaisAntiga();
+}
+
+//--------------------------------------------------------------------------------------------------
+// função pra remover tarefa por índice
+function removerPorIndice() {
+    const index = parseInt(prompt("Digite o índice da tarefa"));
+
+    const tarefa = minhaLista.removeAtIndex(index);
+
+    if (!tarefa) {
+        alert("Índice inválido");
+        return;
+    }
+
+    alert("Tarefa removida: " + tarefa.descricao);
+    atualizarLista();
+}
+
+//--------------------------------------------------------------------------------------------------
+// Função para atualizar a exibição da lista
+function atualizarLista() {
+    const listaTarefas = document.getElementById("list_listadeTarefas");
+    const lblTarefas = document.getElementById("lblmostraTarefas");
+
+    listaTarefas.innerHTML = "";
+
+    if (!minhaLista.isEmpty()) {
+        lblTarefas.innerHTML = "Lista de Tarefas";
+
+        for (const tarefa of minhaLista) {
+            const novaLinha = document.createElement("li");
+            novaLinha.innerHTML = tarefa.toString();
+            listaTarefas.appendChild(novaLinha);
+        }
+    } else {
         lblTarefas.innerHTML = "Lista de Tarefas Vazia";
-      }
-      
- }
- 
+    }
+}
--- atvd14 DEQUE/models/LinkedList.js
+++ atvd14 DEQUE/models/LinkedList.js
@@ -1,9 +1,7 @@
-//models/LinkdList.js
 class LinkedList{
     #head;
     #tail;
     #qtd;
-
 
     constructor(){
         this.#head = null;
@@ -11,105 +9,216 @@ class LinkedList{
         this.#qtd = 0;
     }
 
-
     isEmpty(){
-        return this.#head=== null;
+        return this.#head === null;
     }
 
+    getFirst(){
+        return this.isEmpty() ? null : this.#head.dado;
+    }
 
+    //--------------------------------------------------------------------------------------------------
     addFirst(novoDado){
         const novoNo = new No(novoDado);
-         if(this.isEmpty())// vazia
+
+        if(this.isEmpty())
             this.#tail = novoNo;
         else{
-           novoNo.proximo = this.#head;
-           this.#head.anterior = novoNo;
-
-
+            novoNo.proximo = this.#head;
+            this.#head.anterior = novoNo;
         }
+
         this.#head = novoNo;
         this.#qtd++;
         return true;
     }
 
-
+    //--------------------------------------------------------------------------------------------------
     addLast(novoDado){
         const novoNo = new No(novoDado);
-        if(this.isEmpty())// vazia
+
+        if(this.isEmpty())
             this.#head = novoNo;
         else{
-           novoNo.anterior = this.#tail;
-           this.#tail.proximo = novoNo;
-
-
+            novoNo.anterior = this.#tail;
+            this.#tail.proximo = novoNo;
         }
+
         this.#tail = novoNo;
         this.#qtd++;
         return true;
     }
 
+    //--------------------------------------------------------------------------------------------------
+    addAtIndex(dado, index){
+        if(index <= 0) return this.addFirst(dado);
+        if(index >= this.#qtd) return this.addLast(dado);
 
+        let atual = this.#head;
+
+        for(let i = 0; i < index; i++){
+            atual = atual.proximo;
+        }
+
+        const novoNo = new No(dado);
+
+        novoNo.proximo = atual;
+        novoNo.anterior = atual.anterior;
+
+        atual.anterior.proximo = novoNo;
+        atual.anterior = novoNo;
+
+        this.#qtd++;
+        return true;
+    }
+
+    //--------------------------------------------------------------------------------------------------
     removeFirst(){
-        if(this.isEmpty())
-            return null;
+        if(this.isEmpty()) return null;
+
         const dadoRemovido = this.#head.dado;
         this.#head = this.#head.proximo;
-        if(this.#head==null)
-         this.#tail = null;
+
+        if(this.#head == null)
+            this.#tail = null;
         else
-            this.#head .anterior = null
+            this.#head.anterior = null;
+
         this.#qtd--;
         return dadoRemovido;
     }
 
-
+    //--------------------------------------------------------------------------------------------------
     removeLast(){
-       if(this.isEmpty())
-            return null;
+        if(this.isEmpty()) return null;
+
         const dadoRemovido = this.#tail.dado;
         this.#tail = this.#tail.anterior;
-        if(this.#tail==null)
-         this.#head = null;
+
+        if(this.#tail == null)
+            this.#head = null;
         else
-            this.#tail.proximo = null
+            this.#tail.proximo = null;
+
         this.#qtd--;
         return dadoRemovido;
     }
-    
 
+    //------------------------------------------------------
+    removeAtIndex(index){
+        if(this.isEmpty()) return null;
+
+        if(index === 0) return this.removeFirst();
+        if(index === this.#qtd - 1) return this.removeLast();
+
+        let atual = this.#head;
+
+        for(let i = 0; i < index; i++){
+            atual = atual.proximo;
+        }
+
+        atual.anterior.proximo = atual.proximo;
+        atual.proximo.anterior = atual.anterior;
+
+        this.#qtd--;
+        return atual.dado;
+    }
+
+    //--------------------------------------------------------------------------------------------------
+    // ordenado por prioridade
+    addOrdenado(tarefa) {
+        if (this.isEmpty()) {
+            return this.addFirst(tarefa);
+        }
+
+        let atual = this.#head;
+        let pos = 0;
+
+        while (atual && atual.dado.prioridade <= tarefa.prioridade) {
+            atual = atual.proximo;
+            pos++;
+        }
+
+        return this.addAtIndex(tarefa, pos);
+    }
+//---------------'-----------------------------------------------------------------------------------
+    // remover com tempo
+    removeFirstComTempo(){
+        if(this.isEmpty()) return null;
+
+        const tarefa = this.#head.dado;
+
+        const agora = new Date();
+        const inicio = new Date(`${converterDataFormatoISO8601(tarefa.data)}T${tarefa.hora}`);
+
+        const diff = agora - inicio;
+
+        const minutos = Math.floor(diff / 60000);
+        const horas = Math.floor(minutos / 60);
+        const dias = Math.floor(horas / 24);
+
+        alert(`Concluída em ${dias} dias, ${horas % 24} horas e ${minutos % 60} minutos`);
+
+        return this.removeFirst();
+    }
+
+    //------------------------------------------------------------------------------------------
+    // tarefa mais antiga
+    tarefaMaisAntiga(){
+        if(this.isEmpty()){
+            return alert("Lista de Tarefas Vazia");
+        }
+
+        let atual = this.#head;
+        let maisAntiga = atual.dado;
+
+        while(atual){
+            if(
+                isMenor(
+                    atual.dado.data,
+                    atual.dado.hora,
+                    maisAntiga.data,
+                    maisAntiga.hora
+                )
+            ){
+                maisAntiga = atual.dado;
+            }
+
+            atual = atual.proximo;
+        }
+
+        alert("Tarefa mais antiga: " + maisAntiga.toString());
+    }
 
     get length(){
         return this.#qtd;
     }
+//--------------------------------------------------------------------------------------------------
+    [Symbol.iterator]() {
+        let noAtual = this.#head;
 
-
-    //-------------------------------------
-//Quando um objeto tem um iterator, ele pode ser iterado com construções como [ for(const item of minhaLista)*/
-
-
-[Symbol.iterator]() {         
-    let noAtual = this.#head;
-          return {
+        return {
             next: function() {
-              if (noAtual!==null) {
-                let valor = noAtual.dado;
-                noAtual = noAtual.proximo;
-                return { value: valor, done: false };
-              } else {
-                return { done: true };
-              }
+                if (noAtual !== null) {
+                    let valor = noAtual.dado;
+                    noAtual = noAtual.proximo;
+                    return { value: valor, done: false };
+                } else {
+                    return { done: true };
+                }
             }
-          };
-        }
-  //—----------------
+        };
+    }
+//--------------------------------------------------------------------------------------------------
     toString() {
-          let Noatual = this.#head
-          while(Noatual!=null){
-          retorno+="| " + noAtual.dado;
-          Noatual=Noatual.proximo;
+        let atual = this.#head;
+        let retorno = "";
+
+        while(atual != null){
+            retorno += "| " + atual.dado.toString() + " ";
+            atual = atual.proximo;
         }
-   //---------------- 
-     }     
- }
 
-
+        return retorno;
+    }
+}
